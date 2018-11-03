563e45c2c989cf3724f8f77d86c356f1848a4636
jessyy <wgq287676521@qq.com>
2018-11-03 23:55:53 +0800

    显示登陆用户信息

--- src/main/java/com/jesse/onecake/common/config/security/MvcConfig.java
+++ src/main/java/com/jesse/onecake/common/config/security/MvcConfig.java
@@ -10,7 +10,7 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("index");
         registry.addViewController("/register").setViewName("register");
-        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/").setViewName("index");
         registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");
     }
