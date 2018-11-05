6ffcdb15de31d7f0e788da6128e1ca75dba4f168
jessyy <wgq287676521@qq.com>
2018-11-05 14:12:05 +0800

    增加数量显示实时更新

--- src/main/java/com/jesse/onecake/controller/CartController.java
+++ src/main/java/com/jesse/onecake/controller/CartController.java
@@ -3,9 +3,12 @@ package com.jesse.onecake.controller;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.jesse.onecake.biz.CartBiz;
+import com.jesse.onecake.common.config.security.UserUtils;
 import com.jesse.onecake.controller.base.BaseController;
 import com.jesse.onecake.entity.Cart;
+import com.jesse.onecake.entity.User;
 import com.jesse.onecake.mapper.CartDetailMapper;
+import com.jesse.onecake.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,10 +23,21 @@ import java.util.Map;
 public class CartController extends BaseController<CartBiz,Cart> {
 
     @Autowired private CartDetailMapper cartDetailMapper;
+    @Autowired private UserMapper userMapper;
 
     @RequestMapping(value = "/addCart",method = RequestMethod.POST)
     public @ResponseBody
     Object addCart (String cakeId, String quantity) {
         return this.biz.addCart(cakeId,Integer.valueOf(quantity));
+    }
+
+    @RequestMapping(value = "/cartCount",method = RequestMethod.POST)
+    public @ResponseBody
+    Object cartCount () {
+        Map<String,Integer> map = new HashMap<>();
+        User user = userMapper.findByName(UserUtils.getUserName());
+        Integer count = cartDetailMapper.selectCountCartDetailByUserId(user.getId().toString());
+        map.put("count",count);
+        return map;
     }
 }
