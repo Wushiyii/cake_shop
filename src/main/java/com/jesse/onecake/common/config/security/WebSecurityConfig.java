48d13087b2dc9c82436b2b8e6687e7fbafa59328
jessyy <wgq287676521@qq.com>
2018-11-11 16:19:11 +0800

    修改登陆页面、error页面..

--- src/main/java/com/jesse/onecake/common/config/security/WebSecurityConfig.java
+++ src/main/java/com/jesse/onecake/common/config/security/WebSecurityConfig.java
@@ -35,6 +35,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
             .logout()
+                .logoutSuccessUrl("/cake/")
                 .permitAll();
     }
 
